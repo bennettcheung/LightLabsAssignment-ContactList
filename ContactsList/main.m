ea53fd5226de6f2fa726a131924e893188462ec1
HighGroundLover <kclarkoneil@yahoo.ca>
2018-07-31 14:05:27 -0700

    Initial Commit

---
+++ ContactsList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContactsList
+//
+//  Created by Kit Clark-O'Neil on 2018-07-31.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
